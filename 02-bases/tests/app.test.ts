f90f8892135d72d51bb714ccbc2d0faab9d6ee8b
Marcos <mdiannibelli@gmail.com>
2024-09-11 16:13:38 -0300

    testing jest

---
+++ 02-bases/tests/app.test.ts
@@ -0,0 +1,18 @@
+// A A A
+// Arrange - Act - Assert
+
+describe('Test in the App File', () => {
+    test('should be 30', () => {
+        expect(true).toBe(true)
+
+        // 1. Arrange
+        const num1 = 10;
+        const num2 = 20;
+
+        // 2. Act
+        const result = num1 + num2;
+
+        // 3. Assert
+        expect(result).toBe(30);
+    })
+})
