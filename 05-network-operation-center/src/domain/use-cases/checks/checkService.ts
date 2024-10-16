74e97872e84d6a7545a19bb12173233c2c38c6b3
Marcos <mdiannibelli@gmail.com>
2024-10-16 18:59:49 -0300

    enviroments

--- 05-network-operation-center/src/domain/use-cases/checks/checkService.ts
+++ 05-network-operation-center/src/domain/use-cases/checks/checkService.ts
@@ -2,11 +2,11 @@ import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
 import { LogRepository } from "../../repository/log.repository";
 
 interface CheckServiceUseCase {
-    execute( url : string ): Promise<boolean>;
+    execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = ( error: string ) => void;
+type SuccessCallback = () => void | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 
 export class CheckService implements CheckServiceUseCase {
@@ -15,26 +15,26 @@ export class CheckService implements CheckServiceUseCase {
     private logRepository: LogRepository
 
     constructor(successCallback: SuccessCallback, errorCallback: ErrorCallback, logRepository: LogRepository) {
-        this.successCallback = successCallback; 
+        this.successCallback = successCallback;
         this.errorCallback = errorCallback;
         this.logRepository = logRepository;
     }
 
-    public async execute( url: string ):Promise<boolean> {
+    public async execute(url: string): Promise<boolean> {
         try {
             const req = await fetch(url);
-            if(!req.ok) {
+            if (!req.ok) {
                 throw new Error(`Error on check service ${url}`);
             }
 
             this.logRepository.saveLog(new LogEntity(`Service ${url} working`, LogSeverityLevel.low))
-            this.successCallback()
+            this.successCallback && this.successCallback()
             //console.log(`${url} is ok`)
             return true;
         } catch (error) {
             //console.log(`${error}`)
             this.logRepository.saveLog(new LogEntity(`${error}`, LogSeverityLevel.high))
-            this.errorCallback(`${error}`)
+            this.errorCallback && this.errorCallback(`${error}`)
             return false;
         }
     }
--- 05-network-operation-center/src/presentation/server.ts
+++ 05-network-operation-center/src/presentation/server.ts
@@ -1,7 +1,10 @@
+import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/checkService"
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource"
 import { LogRepositoryImplementation } from "../infrastructure/repositories/log-impl.repository"
 import { CronService } from "./cron/cronService"
+
+import 'dotenv/config';
 
 const fileSystemRepository = new LogRepositoryImplementation(
     new FileSystemDatasource()
@@ -9,7 +12,7 @@ const fileSystemRepository = new LogRepositoryImplementation(
 
 export class Server {
     public static start() {
-        console.log("Server started...")
+        console.log(`Server started at port ${envs.PORT}...`)
 
         CronService.createJob(
             '* * * * * *',
@@ -18,7 +21,7 @@ export class Server {
                     () => console.log("success"),
                     (error) => console.log(error),
                     fileSystemRepository
-                    
+
                 ).execute('https://google.com')
                 //new CheckService().execute('http://localhost:3000')
             }
